7f82ca065c2074b61c259031f6b5f38f76452ff2
Dev <dev@Mac.versumdata.com>
2026-03-29 19:11:40 +0200

    update semifuncional

---
+++ gemini-bridge/frontend/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: '../public',
+  output: 'static',
+  build: {
+    format: 'file',
+  }
+});
